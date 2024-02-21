c92a42dff2659cfedbb5d6c5451e93c57e97d073
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2024-02-21 14:24:23 +0300

    Create Setup.cs

---
+++ sample/Yapoml.Playwright.Sample/Setup.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Yapoml.Playwright.Sample;
+
+[SetUpFixture]
+class Setup
+{
+    [OneTimeSetUp]
+    public void InstallPlaywright()
+    {
+        Microsoft.Playwright.Program.Main(new string[] { "install" });
+    }
+}
